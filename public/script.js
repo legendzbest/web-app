0af3347cf6fe919f70bb0a3a543047c31b117d4b
Jhony <jhonyalkhoury8@gmail.com>
2023-10-27 00:05:20 -0600

    add button and counter for clicks in script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
+const button=document.getElementById("Button");
+const counter=document.getElementById("Counter");
 let number=0;
+button.addEventListener("click", ()=>{
+    number+=1;
+    counter.textContent=number;
+ });
