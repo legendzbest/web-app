2eb9ce02b59c36b2fdeaf0f208d94102be98d24d
Jhony <jhonyalkhoury8@gmail.com>
2023-10-26 23:27:16 -0600

    Updated files to fit instructions in the ReadME

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
-
+const port=3000;
+app.get('/',(request,response)=>{
+response.send("hello habibi");
+});
+app.listen(port, ()=>{
+    console.log(`server is on port ${port}`);
+});
 // write the code here to setup the server and get it to run on PORT 3000
-// host on heroku
+// host on heroku   
