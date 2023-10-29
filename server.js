af235c9fd8057ad978112401cb91253e26a6b793
Jhony <jhonyalkhoury8@gmail.com>
2023-10-28 18:19:01 -0600

    update styles and index

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port=3000;
 app.get('/',(request,response)=>{
-response.send("hello habibi");
+response.send();
 });
 app.listen(port, ()=>{
     console.log(`server is on port ${port}`);
